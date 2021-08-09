afd79786299b10773235eacca0637a9c1614b8ef
nhatvu148 <nhatvu148@gmail.com>
2021-08-09 14:06:43 +0900

    invoke from js

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,18 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{command};
+
+#[command]
+fn my_custom_command() {
+  println!("I was invoked from JS!");
+}
+
 fn main() {
+  let msg = String::from("Hello WORLD!");
+  println!("Message from Rust: {}", msg);
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![my_custom_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
