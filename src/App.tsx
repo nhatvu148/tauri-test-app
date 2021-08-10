45b48a1c2dd2327fee1266e952e3c54cb780e929
nhatvu148 <nhatvu148@gmail.com>
2021-08-10 19:27:27 +0900

    fixing ui

--- src/App.tsx
+++ src/App.tsx
@@ -43,130 +43,156 @@ const App = () => {
   const [counter, setCounter] = useState(0);
   const [clientPort, setClientPort] = useState(0);
   const [serverPort, setServerPort] = useState(0);
+  const [isPortChanged, setIsPortChanged] = useState(false);
+  const [isServerOn, setIsServerOn] = useState(false);
 
   return (
     <div className="main-content">
       <Container className="mt-5" fluid>
         <Card>
           <CardBody>
-            <Row>
-              <Col md="8">
+            <Row align="center">
+              <div className="col-5">
                 <Form>
                   <FormGroup className="row">
                     <Label
                       className="form-control-label"
                       htmlFor="example-number-input"
-                      md="4"
+                      md="5"
                     >
                       Port Number
                     </Label>
-                    <Col md="6">
+                    <Col md="7">
                       <Input
-                        defaultValue="23"
+                        value={clientPort}
+                        onChange={(event) => {
+                          setClientPort(Number(event.target.value));
+                          setIsPortChanged(true);
+                        }}
                         id="example-number-input"
                         type="number"
                       />
                     </Col>
                   </FormGroup>
                 </Form>
-              </Col>
-              <Col md="4">
-                {serverPort === 0 ? (
-                  <Button
+              </div>
+              <div className="col-4" style={{ justifyContent: "center" }}>
+                {isPortChanged && isServerOn ?
+                  (<Button
                     color="success"
                     type="button"
+                    style={{ marginBottom: 10 }}
                     onClick={() => {
-                      const newServerPort = 4000;
-                      const newCientPort = 50505;
-                      setServerPort(newServerPort);
-                      setClientPort(newCientPort);
-
                       invoke("start_server", {
-                        port: newCientPort,
-                        portProd: newServerPort,
+                        port: clientPort,
+                        portProd: serverPort,
                       });
+                      setIsPortChanged(false);
                     }}
                   >
-                    Start Server
-                  </Button>
-                ) : (
-                  <Button
-                    color="danger"
-                    type="button"
-                    onClick={() => {
-                      const newServerPort = 0;
-                      const newCientPort = 0;
-                      setServerPort(newServerPort);
-                      setClientPort(newCientPort);
+                    Restart Server
+                  </Button>)
+                  : !isServerOn ? (
+                    <Button
+                      color="success"
+                      type="button"
+                      style={{ marginBottom: 10 }}
+                      onClick={() => {
+                        const newServerPort = 4000;
+                        const newCientPort = 50505;
+                        setServerPort(newServerPort);
+                        setClientPort(newCientPort);
+                        setIsServerOn(true);
 
-                      invoke("stop_server");
-                    }}
-                  >
-                    Stop Server
-                  </Button>
-                )}
-
+                        invoke("start_server", {
+                          port: newCientPort,
+                          portProd: newServerPort,
+                        });
+                      }}
+                    >
+                      Start Server
+                    </Button>
+                  ) : (
+                    <Button
+                      color="danger"
+                      type="button"
+                      style={{ marginBottom: 10 }}
+                      onClick={() => {
+                        invoke("stop_server");
+                        setIsServerOn(false);
+                      }}
+                    >
+                      Stop Server
+                    </Button>
+                  )}
                 <Button
                   color="info"
                   type="button"
+                  style={{ marginBottom: 20 }}
                   onClick={async () => {
-                    if (serverPort === 0) {
+                    if (!isServerOn) {
                       alert("Server is not running!");
                     } else {
                       await open(`http://localhost:${clientPort}`);
                     }
                   }}
                 >
-                  Launch JMU-DT Web
+                  Launch Browser
                 </Button>
+              </div>
+              <div className="col-3" style={{ justifyContent: "center" }}>
+                <Button
+                  color="success"
+                  type="button"
+                  style={{ marginBottom: 10 }}
+                  onClick={() => {
+                    invoke("my_custom_command2", { invokeMessage: "Hello!" });
+                    invoke("my_custom_command3").then((message) => {
+                      setMessage(message as string);
+                      setCounter((prev) => prev + 1);
+                      console.log(message);
+                    });
+                  }}
+                >
+                  Click me
+                </Button>
+                <Button
+                  color="success"
+                  type="button"
+                  style={{ marginBottom: 20 }}
+                  onClick={async () => {
+                    const name = await getName();
+                    console.log(name);
+                    writeText("Kyoko");
+                    const text = await readText();
+                    console.log(text);
+
+                    const files = await readDir("./");
+                    console.log(files);
+                    const content = await readTextFile("./Cargo.toml");
+                    console.log(content);
+
+                    setCounter(0);
+
+                    setMessage("");
+                  }}
+                >
+                  Clear
+                </Button>
+              </div>
+            </Row>
+            <Row align="center">
+              <Col>
+                <h2>
+                  {!isPortChanged || serverPort === 0
+                    ? "Server stopped!"
+                    : `JMU-DT Web Server is running on port ${clientPort}!`}
+                </h2>
+                <h2>
+                  {message} {counter ? `${counter}!` : ""}
+                </h2>
               </Col>
             </Row>
-
-            <Button
-              color="success"
-              type="button"
-              onClick={() => {
-                invoke("my_custom_command2", { invokeMessage: "Hello!" });
-                invoke("my_custom_command3").then((message) => {
-                  setMessage(message as string);
-                  setCounter((prev) => prev + 1);
-                  console.log(message);
-                });
-              }}
-            >
-              Click me
-            </Button>
-            <Button
-              color="success"
-              type="button"
-              onClick={async () => {
-                const name = await getName();
-                console.log(name);
-                writeText("Kyoko");
-                const text = await readText();
-                console.log(text);
-
-                const files = await readDir("./");
-                console.log(files);
-                const content = await readTextFile("./Cargo.toml");
-                console.log(content);
-
-                setCounter(0);
-
-                setMessage("");
-              }}
-            >
-              Clear
-            </Button>
-
-            <h2>
-              {serverPort === 0
-                ? "Server stopped!"
-                : `JMU-DT Web Server is running on port ${clientPort}!`}
-            </h2>
-            <h2>
-              {message} {counter ? `${counter}!` : ""}
-            </h2>
           </CardBody>
         </Card>
       </Container>
